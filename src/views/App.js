23c9e89f51783e2b9262f5a30b3947568529db87
Matt Tingen <matt.tingen@willowtreeapps.com>
2018-06-06 13:23:45 -0400

    Use emotion for css

--- src/views/App.js
+++ src/views/App.js
@@ -1,16 +1,43 @@
+import { css, keyframes } from 'emotion';
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+const spin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const classes = {
+  container: css`
+    text-align: center;
+  `,
+  logo: css`
+    animation: ${spin} infinite 20s linear;
+    height: 80px;
+  `,
+  header: css`
+    background-color: #222;
+    height: 150px;
+    padding: 20px;
+    color: white;
+  `,
+  title: css`
+    font-size: 1.5em;
+  `,
+  intro: css`
+    font-size: large;
+  `,
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={classes.container}>
+        <header className={classes.header}>
+          <img src={logo} className={classes.logo} alt="logo" />
+          <h1 className={classes.title}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={classes.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
