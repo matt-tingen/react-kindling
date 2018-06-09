be44dafc02394a17434c469996f48626dfb5edeb
Matt Tingen <matt.tingen@gmail.com>
2018-06-09 15:07:15 -0400

    Add basic delete from list

--- src/views/Items.js
+++ src/views/Items.js
@@ -1,16 +1,33 @@
 import React from 'react'
+import styled from 'react-emotion'
 import { connect } from 'react-redux'
 import { firestoreConnect, isEmpty, isLoaded } from 'react-redux-firebase'
+import { withHandlers, withProps } from 'recompose'
 import { compose } from 'redux'
 
-const Items = ({ items }) => {
+const MiniDeleteButton = withProps({
+  type: 'button',
+  children: 'X',
+})(styled.button`
+  margin: 10px;
+  color: red;
+  font-weight: bold;
+`)
+
+const Items = ({ items, deleteItem }) => {
   const itemsList = !isLoaded(items)
     ? 'Loading'
     : isEmpty(items)
       ? 'Item list is empty'
-      : Object.keys(items).map((key, id) => (
-          <li key={key}>{items[key].name}</li>
-        ))
+      : Object.keys(items).map(
+          id =>
+            items[id] && (
+              <li key={id}>
+                {items[id].name}
+                <MiniDeleteButton onClick={() => deleteItem(id)} />
+              </li>
+            ),
+        )
   return (
     <div>
       <h1>Items</h1>
@@ -19,9 +36,14 @@ const Items = ({ items }) => {
   )
 }
 
+const collection = 'items'
+
 export default compose(
-  firestoreConnect(['items']),
+  firestoreConnect([collection]),
   connect(state => ({
     items: state.firestore.data.items,
   })),
+  withHandlers({
+    deleteItem: ({ firestore }) => doc => firestore.delete({ collection, doc }),
+  }),
 )(Items)
