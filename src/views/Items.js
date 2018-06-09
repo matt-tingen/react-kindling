99e4ca1c8c2b25a7f7a324df7aec13cd537f7b49
Matt Tingen <matt.tingen@gmail.com>
2018-06-09 16:25:46 -0400

    Fix bare text in <ul>

--- src/views/Items.js
+++ src/views/Items.js
@@ -4,23 +4,27 @@ import firestoreList from '../hocs/firestoreList'
 import MiniDeleteButton from './MiniDeleteButton'
 
 const Items = ({ docs, deleteDoc }) => {
-  const itemsList = !isLoaded(docs)
-    ? 'Loading'
-    : isEmpty(docs)
-      ? 'Item list is empty'
-      : Object.keys(docs).map(
-          id =>
-            docs[id] && (
-              <li key={id}>
-                {docs[id].name}
-                <MiniDeleteButton onClick={() => deleteDoc(id)} />
-              </li>
-            ),
-        )
+  const itemsList = !isLoaded(docs) ? (
+    'Loading'
+  ) : isEmpty(docs) ? (
+    'Item list is empty'
+  ) : (
+    <ul>
+      {Object.keys(docs).map(
+        id =>
+          docs[id] && (
+            <li key={id}>
+              {docs[id].name}
+              <MiniDeleteButton onClick={() => deleteDoc(id)} />
+            </li>
+          ),
+      )}
+    </ul>
+  )
   return (
     <div>
       <h1>Items</h1>
-      <ul>{itemsList}</ul>
+      {itemsList}
     </div>
   )
 }
