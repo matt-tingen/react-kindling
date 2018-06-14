2618b469797c534b80dfbe8673f0815372575413
Matt Tingen <matt.tingen@willowtreeapps.com>
2018-06-14 16:49:28 -0400

    Add basic styling for active nav link

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,17 +1,26 @@
+import { css } from 'emotion'
 import React from 'react'
 import styled from 'react-emotion'
-import { Link } from 'react-router-dom'
+import { NavLink as NavLink_ } from 'react-router-dom'
+import { withProps } from 'recompose'
 
 const Nav = styled.nav`
   margin-bottom: 1em;
 `
 
-const NavLink = styled(Link)`
-  margin-left: 1em;
+const activeClass = css`
+  font-weight: bold;
 `
+
+const NavLink = withProps({ activeClassName: activeClass })(styled(NavLink_)`
+  margin-left: 1em;
+`)
+
 const NavBar = () => (
   <Nav>
-    <NavLink to="/">Home</NavLink>
+    <NavLink to="/" exact>
+      Home
+    </NavLink>
     <NavLink to="/add">Add</NavLink>
     <NavLink to="/auth">Auth</NavLink>
   </Nav>
