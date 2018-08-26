ed5d207577afe09845f422f4d29567543722891b
Matt Tingen <matt.tingen@gmail.com>
2018-08-26 16:20:52 -0400

    Type Router

--- src/views/Router.tsx
+++ src/views/Router.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import authOnly from '../hocs/authOnly'
 import AddItem from './AddItem'
 import Auth from './Auth'
 import Items from './Items'
 
-const Router = () => (
+const Router: React.SFC = () => (
   <Switch>
     <Route exact path="/" component={authOnly(Items)} />
     <Route path="/add" component={authOnly(AddItem)} />
