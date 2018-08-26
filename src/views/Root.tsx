2177210de5600e7b1b0c8a1d2412446ff9d3591e
Matt Tingen <matt.tingen@gmail.com>
2018-08-26 10:13:40 -0400

    Type Root

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import * as React from 'react'
 import { hot } from 'react-hot-loader'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'unstated'
 import App from './App'
 
-const Root = () => (
+const Root: React.SFC = () => (
   <Provider>
     <Router>
       <App />
