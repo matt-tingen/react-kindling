729decf163aac377362213b23393c078166e708c
Matt Tingen <matt.tingen@willowtreeapps.com>
2018-08-27 11:21:02 -0400

    Improve asFunction types

--- src/utils/asFunction.ts
+++ src/utils/asFunction.ts
@@ -1,4 +1,7 @@
-const asFunction = <T>(value: T) =>
-  typeof value === 'function' ? value : () => value
+function asFunction<F extends Function>(value: F): F
+function asFunction<T>(value: T): () => T
+function asFunction(value: any) {
+  return typeof value === 'function' ? value : () => value
+}
 
 export default asFunction
